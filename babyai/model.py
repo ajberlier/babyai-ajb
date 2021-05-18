a72501d13c9149ee6459e24cfffcd49ac695f8f0
gkebe <mb88814@umbc.edu>
2021-05-18 04:21:48 -0400

    Update model.py

--- babyai/model.py
+++ babyai/model.py
@@ -190,9 +190,9 @@ class ACModel(nn.Module, babyai.rl.RecurrentACModel):
         if 'pixel' in self.arch:
             x /= 256.0
         x = self.image_conv(x)
-        x_pool = self.obs_pool(x)
+        x_pool = self.obs_pool(x).view(x.shape[0], -1)
         instr_embedding = self._get_instr_embedding(obs.instr)
-        self.latents = self.get_att_instr(self.init_obs, None, instr_embedding, x)
+        self.latents = self.get_att_instr(self.init_obs, None, instr_embedding, x_pool)
 
     def add_heads(self):
         '''
